fd57729d05e398224c1f1c966b64c9973b3600b6
thvjmren <ranamm-bb209@code.edu.az>
2026-08-06 12:49:44 +0400

    Fix N+1 Query Problem

--- src/main/java/com/rana/library_api/repository/BookRepository.java
+++ src/main/java/com/rana/library_api/repository/BookRepository.java
@@ -1,9 +1,28 @@
 package com.rana.library_api.repository;
 
 import com.rana.library_api.entity.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface BookRepository extends JpaRepository<Book, Long>,
         JpaSpecificationExecutor<Book> {
+
+    @Override
+    @EntityGraph(attributePaths = {"author", "categories"})
+    Page<Book> findAll(Pageable pageable);
+
+    @Override
+    @EntityGraph(attributePaths = {"author", "categories"})
+    List<Book> findAll(Specification<Book> specification);
+
+    @Override
+    @EntityGraph(attributePaths = {"author", "categories"})
+    Optional<Book> findById(Long id);
 }
